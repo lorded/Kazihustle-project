a24037d40c678099ffbdc71dd5ae7d456374cd14
dishon maina <mynadishon@gmail.com>
2024-10-01 19:15:20 +0300

    Update util.php
    Update API key credentials

--- util.php
+++ util.php
@@ -7,12 +7,15 @@
         // Live f3a89f84ee2a451290dc1e18d982dcfcea144f54b6ff89dea3c2dbf52421567e
         // Sandbox dea58b52342056c421578794e617990d909f0597c067372a06cbb5b1cde314ee
 
+        // Mobipower - atsk_ea0dab7dd1c235ee27a5b9d3ac887219a4c2cdf853bd5819115571ce29ba05fcda2b042e
 
-        static  $API_KEY = "79dc1cd13d1c9a2aa5045c171302d1cd0cd43ca1082aa56c6893f8b1c66a41f3"; 
 
-        static $API_USERNAME = "ecosolutions"; //sandbox
 
-        static $COMPANY_NAME = "Greenlifeke";
+
+        static  $API_KEY = "atsk_ea0dab7dd1c235ee27a5b9d3ac887219a4c2cdf853bd5819115571ce29ba05fcda2b042e"; 
+
+        static $API_USERNAME = "sandbox"; //sandbox
+        static $COMPANY_NAME = "63083";
 
         static $COUNTRY_CODE = "254";
 
