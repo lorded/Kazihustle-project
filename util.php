3e0ac7143c30de999234b28aaefbc950884049e1
dishon maina <mynadishon@gmail.com>
2024-10-02 16:16:41 +0300

    Update util.php
    removed Api keys

--- util.php
+++ util.php
@@ -4,18 +4,18 @@
 
         static $GO_TO_MAIN_MENU = "99";
 
-        // Live f3a89f84ee2a451290dc1e18d982dcfcea144f54b6ff89dea3c2dbf52421567e
-        // Sandbox dea58b52342056c421578794e617990d909f0597c067372a06cbb5b1cde314ee
+        // Live f
+        // Sandbox 
 
-        // Mobipower - atsk_ea0dab7dd1c235ee27a5b9d3ac887219a4c2cdf853bd5819115571ce29ba05fcda2b042e
-
-
+        // Mobipower - 
 
 
-        static  $API_KEY = "atsk_ea0dab7dd1c235ee27a5b9d3ac887219a4c2cdf853bd5819115571ce29ba05fcda2b042e"; 
 
-        static $API_USERNAME = "sandbox"; //sandbox
-        static $COMPANY_NAME = "63083";
+
+        static  $API_KEY = ""; 
+
+        static $API_USERNAME = ""; //sandbox
+        static $COMPANY_NAME = "";
 
         static $COUNTRY_CODE = "254";
 
